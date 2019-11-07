e7b451e1557d10f16576f534d4e40af8f62b65b6
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2019-11-07 13:39:30 -0600

    Link time to plan in local storage

--- main.js
+++ main.js
@@ -8,59 +8,93 @@ timeHour = (timeHour > 12)? timeHour - 12: timeHour;
 timeHour = (timeHour == "00")?12 : timeHour;
 }
 
+// Store user input in local Storage
+
 $('#btn1').click(function() {
   if($('#text1').val()) {
     localStorage.removeItem('text1');
   }
-  localStorage.setItem("text1", $('#text1').val());
+
+  const hour1 = $('#time1').text();
+  const hour1String = JSON.stringify(hour1);
+  
+  localStorage.setItem(hour1, $('#text1').val());
 });
 
 $('#btn2').click(function() {
   if($('#text2').val()) {
     localStorage.removeItem('text2');
   }
-  localStorage.setItem("text2", $('#text2').val());
+
+  const hour2 = $('#time2').text();
+  const hour2String = JSON.stringify(hour2);
+
+  localStorage.setItem(hour2, $('#text2').val());
 });
 
 $('#btn3').click(function() {
   if($('#text3').val()) {
     localStorage.removeItem('text3');
   }
-  localStorage.setItem("text3", $('#text3').val());
+
+  const hour3 = $('#time3').text();
+  const hour3String = JSON.stringify(hour3);
+
+  localStorage.setItem(hour3, $('#text3').val());
 });
 
 $('#btn4').click(function() {
   if($('#text4').val()) {
     localStorage.removeItem('text4');
   }
-  localStorage.setItem("text4", $('#text4').val());
+
+  const hour4 = $('#time4').text();
+  const hour4String = JSON.stringify(hour4);
+
+  localStorage.setItem(hour4, $('#text4').val());
 });
 
 $('#btn5').click(function() {
   if($('#text5').val()) {
     localStorage.removeItem('text5');
   }
-  localStorage.setItem("text5", $('#text5').val());
+
+  const hour5 = $('#time5').text();
+  const hour5String = JSON.stringify(hour5);
+
+  localStorage.setItem(hour5, $('#text5').val());
 });
 
 $('#btn6').click(function() {
   if($('#text6').val()) {
     localStorage.removeItem('text6');
   }
-  localStorage.setItem("text6", $('#text6').val());
+
+  const hour6 = $('#time6').text();
+  const hour6String = JSON.stringify(hour6);
+
+  localStorage.setItem(hour6, $('#text6').val());
 });
 
 $('#btn7').click(function() {
   if($('#text7').val()) {
     localStorage.removeItem('text7');
   }
-  localStorage.setItem("text7", $('#text7').val());
+
+  const hour7 = $('#time7').text();
+  const hour7String = JSON.stringify(hour7);
+
+  localStorage.setItem(hour7, $('#text7').val());
 })
 
 $('#btn8').click(function() {
   if($('#text8').val()) {
     localStorage.removeItem('text8');
   }
+
+  const hour8 = $(hour8).text();
+  const hour8String = JSON.stringify(hour8);
+
   localStorage.setItem("text8", $('#text8').val());
 })
 
@@ -68,6 +102,10 @@ $('#btn9').click(function() {
   if($('#text9').val()) {
     localStorage.removeItem('text9');
   }
-  localStorage.setItem("text9", $('#text9').val());
+
+  const hour9 = $('#time9').text();
+  const hour9String = JSON.stringify(hour9);
+
+  localStorage.setItem(hour9, $('#text9').val());
 })
 
